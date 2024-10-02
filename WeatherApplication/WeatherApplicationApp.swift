ce39b3f1ed3169bd9c3e24dd1b9a5ec8e213026d
Raman Tank <ramantank04022002@gmail.com>
2024-10-02 10:19:07 +0530

    Initial Commit

---
+++ WeatherApplication/WeatherApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApplicationApp.swift
+//  WeatherApplication
+//
+//  Created by Raman Tank on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
